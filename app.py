84161571eed63493d1585510222e4f64b76d51ff
Dipali pradip Lavate <lavatedipali05@gmail.com>
2026-05-14 11:23:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import joblib
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
